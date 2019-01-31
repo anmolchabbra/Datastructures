8e435872a61da95b054d941e3aa76ebbb8841870
Anmol Chabbra <anmolchabbra1@gmail.com>
2019-01-31 22:26:54 +0530

    index are stored in stacks now

--- src/main/java/stack/MatchingBrackets.java
+++ src/main/java/stack/MatchingBrackets.java
@@ -8,11 +8,13 @@ class MatchingBrackets {
 
     public int balanced(String string) {
         Stack<Character> stack = new Stack<Character>();
-        int i = 0; int index=0;
+        Stack<Integer> stack1 = new Stack<Integer>();
+        int i = 0;
         for ( ; i < string.length(); i++ ) {
             char currentc = string.charAt(i);
             if ((currentc == '{') || (currentc == '[') || (currentc == '(')) {
                 stack.push(currentc);
+                stack1.push(i);
             } else if (currentc == '}' || currentc == ']' || currentc == ')') {
                 if (stack.empty()) {
                     return i + 1;
@@ -21,21 +23,22 @@ class MatchingBrackets {
                     char topElement = stack.peek();
                     if (topElement == matchingbracket) {
                         stack.pop();
+                        stack1.pop();
                     } else {
                         return i + 1;
                     }
                 }
             }
         }
-        if (stack.empty()) {
+        if (stack.isEmpty() && stack1.isEmpty()) {
             return -1;
         } else {
-            i = (string.lastIndexOf(stack.peek()) + 1);
+            i = (stack1.peek()+ 1);
            return i;
         }
     }
     public static void main(String[] args) {
-        String string = "foo[][]";
+        String string = "foo{}{";
         MatchingBrackets matcher = new MatchingBrackets();
         int s = matcher.balanced(string);
         if (s == -1) {
